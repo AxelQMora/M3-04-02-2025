6c0f31822ed03b010b5b3fa342d7e73a825eda77
Axel Quintana Mora <axel.quintana.mora@outlook.com>
2025-02-07 21:25:57 -0600

    homework

---
+++ tipos-de-datos.js
@@ -0,0 +1,49 @@
+// Entregable 
+
+//Number
+var age = 42
+console.log(typeof age)
+
+var number2 = -666
+console.log(typeof number2)
+
+var number3 = 0
+console.log(typeof number3)
+
+//String 
+var numberString = 'Veinticinco'
+console.log(typeof number)
+
+var symbol = '"'
+console.log(typeof symbol)
+
+var text = 'null'
+console.log(typeof text)
+
+var answer = 'FALSE'
+console.log(typeof answer)
+
+//Boolean
+var decision = true
+console.log(typeof decision)
+
+
+//Symbol
+var identifier = Symbol("id")
+console.log(typeof identifier)
+
+//bigint
+var bignumber = 1000000000000000000000000000000000000n
+console.log(typeof bignumber)
+
+//null
+var empty = null
+console.log(typeof empty)
+
+//undefined 
+var waiting = undefined
+console.log(typeof waiting)
+
+//nan
+var operation = 0/0
+console.log(typeof operation)
